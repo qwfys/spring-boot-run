6da770ca7363724e66a557299cf0a74b059abde3
lwk <qwfys200@qq.com>
2018-04-18 02:48:36 +0800

    commint files

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # spring-boot-run
 演示mvn spring-boot:run与spring.profiles结合问题。
+
+# run
+
+## default environment
+
+```
+mvn spring-boot:run
+```
+
+## production environment
+
+```
+mvn spring-boot:run -Dspring-boot.run.profiles=prod
+```
+
+## test environment
+
+```
+mvn spring-boot:run -Dspring-boot.run.profiles=test
+```
---
+++ src/test/java/org/qwfys/sample/spring/boot/run/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package org.qwfys.sample.spring.boot.run;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
